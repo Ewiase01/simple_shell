cc96fe8ac8969b2930736bc5d54e72347fb35b28
Kelvin Arhin <Kelvinarhin20@gmail.com>
2023-04-26 22:10:57 +0000

    Header file updated

--- shell.h
+++ shell.h
@@ -16,5 +16,15 @@ int is_interactive(_aww *pew);
 int is_delimiter(char c, char *d);
 int is_alpha(int y);
 int _atoi(char *s);
+#define MAX_COMMAND_LENGTH 1024
+#define MAX_ARGUMENTS 64
+#define PATH_DELIMITER ":"
+
+int _printenv(char **env)
+int env_builtin(char **args, char **env)
+char *find_command(char *command)
+char *build_path(char *dir, char *command)
+int execute_command(char **command, char **envp)
+int built_exit(char **args)
 
 #endif
